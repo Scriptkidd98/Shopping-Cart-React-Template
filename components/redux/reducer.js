d8be998f4e609cd9895eec56a02b7a5006f3418c
Isaac <43118340+Scriptkidd98@users.noreply.github.com>
2022-08-30 15:05:29 +0100

    Add files via upload

--- components/redux/reducer.js
+++ components/redux/reducer.js
@@ -1,23 +1,33 @@
+
 import * as actions from './actionTypes';
 let lastId = 0;
 
-export default function reducer(state = [], action) {
+export default function reducer(state = {cart: {}, user: {userID: null}}, action) {
     switch(action.type) {
         case actions.ITEM_ADDED:
-            return [
-                ...state, {
-                id: ++lastId,
-                description: action.payload.description
-            }];
+            state.cart[++lastId] = action.payload.description;
+            return {
+                ...state
+            }
         case actions.ITEM_REMOVED:
-            return state.filter(item => item.id !== action.payload.id);
+            console.log(action.payload.id);
+            delete state.cart[action.payload.id];
+            return {
+                ...state
+            };
         case actions.LOGGED_IN:
-            return [
-                ...state, {
-                    userID: action.payload.userID
-                }];
+            state.user["userID"] = action.payload.userID;
+            return {
+                ...state,
+                user: {
+                    ...state.user, userID: action.payload.userID
+                }
+            }
         case actions.LOGGED_OUT:
-            return state.filter(user => user.userID !== action.payload.userID);
+            delete state.cart[action.payload.userID];
+            return {
+                ...state
+            };
         default:
             return state;
 
