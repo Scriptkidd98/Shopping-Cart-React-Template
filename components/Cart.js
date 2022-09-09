8058e56b59010362be39f60030478500909b18bc
Isaac <43118340+Scriptkidd98@users.noreply.github.com>
2022-09-09 11:06:25 +0100

    Add files via upload

--- components/Cart.js
+++ components/Cart.js
@@ -1,7 +1,10 @@
 import React, {Component} from 'react';
 import './redux/index';
-import {itemRemoved} from './redux/actions';
+import {itemRemoved, logOut} from './redux/actions';
 import { connect } from 'react-redux';
+import './myStyles.css';
+import {Container, Nav, Row, Col} from "react-bootstrap";
+import 'bootstraps/dist/css/bootstrap.css';
 
 
 class Cart extends Component {
@@ -14,17 +17,50 @@ class Cart extends Component {
     }
     render() {
       return(
-        <div> {this.props.item.user.userID}'s Cart
-          {Object.entries(this.props.item.cart).map(([key, value]) => {
-            return (
-              <React.Fragment key={key}>
-                  <div>
-                    <p>{value}</p>
-                    <button onClick={() => this.props.itemRemoved(key)}>Remove</button>
-                  </div>
-              </React.Fragment>
-            );
-          })}
+        <div> 
+          <div className="body">
+
+              <Container fluid className="">
+                <Row>
+                  <Col xs={12} md={8} lg={8} className="">
+                    <div className="products">
+                      <Container fluid>
+                        <Row>
+                          <Col xs={6} md={6} lg={6}>
+                            <h1>Shopping Cart</h1>
+                          </Col>
+                          <Col xs={6} md={6} lg={6}>
+                            <h2 className="items">{Object.entries(this.props.item.cart).length} items</h2>
+                          </Col>
+                        </Row>
+                      </Container>
+                      <div>
+
+                        {this.props.item.user.userID}'s Cart <button onClick={() => this.props.logOut(this.props.item.user.userID)}>Log Out</button>
+
+                        {Object.entries(this.props.item.cart).map(([key, value]) => {
+                          return (
+                            <React.Fragment key={key}>
+                                <div>
+                                  <p>{value}</p>
+                                  <button onClick={() => this.props.itemRemoved(key)}>Remove</button>
+                                </div>
+                            </React.Fragment>
+                          );
+                        })}
+
+                      </div>
+                    </div>  
+                  </Col>
+                  <Col xs={12} md={4} lg={4} className="">
+                    <div className="checkout">
+                        <h2>Order Summary</h2>
+                    </div>
+                  </Col>
+                </Row>
+              </Container>
+
+          </div>
         </div>
       );
     }
@@ -39,7 +75,8 @@ const mapStateToProps = function(state) {
 
 const mapDispatchToProps = function(dispatch) {
   return {
-    itemRemoved: item => dispatch(itemRemoved(item))
+    itemRemoved: item => dispatch(itemRemoved(item)),
+    logOut: item => dispatch(logOut(item))
   }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(Cart);
