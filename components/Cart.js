5c6e7de8880ec5ddd217651412d10315aacdc60d
Isaac <43118340+Scriptkidd98@users.noreply.github.com>
2022-09-09 17:43:47 +0100

    Add files via upload

--- components/Cart.js
+++ components/Cart.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import './redux/index';
-import {itemRemoved, logOut} from './redux/actions';
+import {itemRemoved, logOut, itemAdded} from './redux/actions';
 import { connect } from 'react-redux';
 import './myStyles.css';
 import {Container, Nav, Row, Col} from "react-bootstrap";
@@ -13,6 +13,7 @@ class Cart extends Component {
       console.log("Props", props.item);
       this.state = {
         item: {},
+        initial: 0,
       }
     }
     render() {
@@ -26,7 +27,7 @@ class Cart extends Component {
 
               <Container fluid className="">
                 <Row>
-                  <Col xs={12} md={12} lg={8} className="">
+                  <Col xs={12} md={12} lg={8} className="products-plain">
                     <div className="products">
                       <Container fluid className="removepadding">
                         <Row>
@@ -34,7 +35,13 @@ class Cart extends Component {
                             <h1>Shopping Cart</h1>
                           </Col>
                           <Col xs={6} md={6} lg={6}>
-                            <h2 className="items">{Object.entries(this.props.item.cart).length} items</h2>
+                            <div className="">
+                              { //Check if message failed
+                                (Object.entries(this.props.item.cart).length > 1)
+                                  ? <h2 className="items"> {Object.entries(this.props.item.cart).length} items </h2> 
+                                  : <h2 className="items"> {Object.entries(this.props.item.cart).length} item </h2>  
+                              }
+                            </div>
                           </Col>
                         </Row>
                       </Container>
@@ -47,13 +54,13 @@ class Cart extends Component {
                               <Col lg={3} md={3} xs={3}>
                                 <p>PRODUCT DETAILS</p>
                               </Col>
-                              <Col lg={3} md={3} xs={3}>
+                              <Col lg={3} md={3} xs={3} className="product-header-each">
                                 <p>PRICE</p>
                               </Col>
-                              <Col lg={3} md={3} xs={3}>
+                              <Col lg={3} md={3} xs={3} className="product-header-each">
                                 <p>QUANTITY</p>
                               </Col>
-                              <Col lg={3} md={3} xs={3}>
+                              <Col lg={3} md={3} xs={3} className="product-header-each">
                                 <p>TOTAL</p>
                               </Col>
                             </Row>
@@ -68,14 +75,17 @@ class Cart extends Component {
                                     <Col lg={3} md={3} xs={3}>
                                       <p>{value}</p>
                                     </Col>
-                                    <Col lg={3} md={3} xs={3}>
-                                      {this.props.item.inventory[value]}
+                                    <Col lg={3} md={3} xs={3} className="product-header-each">
+                                      ${this.props.item.inventory[value]}
                                     </Col>
-                                    <Col lg={3} md={3} xs={3}>
-                                      <p>1</p>
+                                    <Col lg={3} md={3} xs={3} className="product-header-each">
+                                      <span>1</span>
+                                      {/* <span>-</span>
+                                      <span>1</span>
+                                      <span>+</span> */}
                                     </Col>
-                                    <Col lg={3} md={3} xs={3}>
-                                      {this.props.item.inventory[value] * 1}
+                                    <Col lg={3} md={3} xs={3} className="product-header-each">
+                                      ${this.props.item.inventory[value] * 1}
                                     </Col>
                                     <Col lg={3} md={3} xs={3}>
                                       <button onClick={() => this.props.itemRemoved(key)}>Remove</button>
@@ -92,7 +102,7 @@ class Cart extends Component {
                       </p>
                     </div>  
                   </Col>
-                  <Col xs={12} md={12} lg={4} className="">
+                  <Col xs={12} md={12} lg={4} className="order-summary-colored">
                     <div className="checkout">
                         <h2 className="order-summary">Order Summary</h2>
                         <hr></hr>
@@ -116,7 +126,8 @@ const mapStateToProps = function(state) {
 const mapDispatchToProps = function(dispatch) {
   return {
     itemRemoved: item => dispatch(itemRemoved(item)),
-    logOut: item => dispatch(logOut(item))
+    logOut: item => dispatch(logOut(item)),
+    itemAdded: item => dispatch(itemAdded(item))
   }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(Cart);
