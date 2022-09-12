9003d6e51e1663c942d4c17b9f342b0dbabb49cb
Isaac <43118340+Scriptkidd98@users.noreply.github.com>
2022-09-12 15:39:34 +0100

    Add files via upload

--- components/redux/reducer.js
+++ components/redux/reducer.js
@@ -1,12 +1,20 @@
 import * as actions from './actionTypes';
 let lastId = 0;
+let initial = 0;
 
-export default function reducer(state = {cart: {}, user: {userID: null}, inventory: {}}, action) {
+export default function reducer(state = {cart: {}, cartQuantity: {}, user: {userID: null}, inventory: {}}, action) {
     switch(action.type) {
         case actions.ITEM_ADDED:
-            state.cart[++lastId] = action.payload.description;
+            state.cart[++lastId] = action.payload.description
+            state.cartQuantity[action.payload.description] = initial + 1
             return {
-                ...state
+                ...state,
+                cart: {
+                    ...state.cart
+                },
+                cartQuantity: {
+                    ...state.cartQuantity
+                }
             }
         case actions.ITEM_REMOVED:
             delete state.cart[action.payload.id];
@@ -31,6 +39,26 @@ export default function reducer(state = {cart: {}, user: {userID: null}, invento
             return {
                 ...state
             };
+        case actions.INCREASE_CART_QUANTITY:
+            console.log(state)
+            const description = action.payload.description;
+            const quantity = action.payload.quantity;
+            return {
+                ...state,
+                cartQuantity: {
+                    ...state.cartQuantity, [description]: quantity + 1
+                }
+            }
+        case actions.DECREASE_CART_QUANTITY:
+            console.log(state)
+            const descriptions = action.payload.description;
+            const quantitys  = action.payload.quantity;
+            return {
+                ...state,
+                cartQuantity: {
+                    ...state.cartQuantity, [descriptions]: quantitys - 1
+                }
+            }
         default:
             return state;
 
