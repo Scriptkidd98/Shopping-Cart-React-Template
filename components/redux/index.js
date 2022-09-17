e34aaaedfdb55b7238d6e5bbea58fbb10231b836
Isaac <43118340+Scriptkidd98@users.noreply.github.com>
2022-09-17 08:05:17 +0100

    Add files via upload

--- components/redux/index.js
+++ components/redux/index.js
@@ -12,6 +12,7 @@ store.dispatch(logIn("johndoe1234"));
 store.dispatch(updateInventory("Apple", 500));
 store.dispatch(updateInventory("Orange", 300));
 store.dispatch(updateInventory("Fries", 200));
+store.dispatch(updateInventory("Chicken", 200));
 //store.dispatch(itemRemoved(1));
 
 //console.log(store.getState());
